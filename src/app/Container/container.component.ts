37251941b84a10702cf21d585653a50ad44c6d6e
rahulvarmass <rahulvarmass2002@gmail.com>
2024-03-27 14:50:05 +0530

    first commit

---
+++ src/app/Container/container.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'app-Container',
+    templateUrl: './container.component.html',
+    styleUrls: ['./container.component.css']
+})
+
+export class ContainerComponent{
+
+
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContainerComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
